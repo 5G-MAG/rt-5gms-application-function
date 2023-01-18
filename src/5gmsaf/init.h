edbfd365129c4b115d67a7c80f50607ef656f27a
David Waring <david.waring2@bbc.co.uk>
2023-01-18 09:58:23 +0000

    Tidy up header (c++ decoration and extern declarations)

--- src/5gmsaf/init.h
+++ src/5gmsaf/init.h
@@ -12,7 +12,15 @@ https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
 
 #include "ogs-app.h"
 
-int msaf_initialize(void);
-void msaf_terminate(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int msaf_initialize(void);
+extern void msaf_terminate(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
