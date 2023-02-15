373523d2ab16f06ba98e87e83c521dc384c917ba
David Waring <david.waring2@bbc.co.uk>
2023-02-15 14:42:38 +0000

    Add API information headers.

--- subprojects/patch_open5gs.sh
+++ subprojects/patch_open5gs.sh
@@ -18,9 +18,11 @@ patch_cmd=`which patch`
 
 if ! grep -q '/\* rt-5gms-applicatiopn-function patch applied \*/' "$open5gs_src/lib/sbi/server.c"; then
     (cd "$open5gs_src"; "$patch_cmd" -p1 <<EOF
---- open5gs.orig/lib/sbi/server.c	2022-10-28 09:40:30.867621595 +0100
-+++ open5gs/lib/sbi/server.c	2022-10-28 09:41:38.004798078 +0100
-@@ -30,9 +30,9 @@
+diff --git a/lib/sbi/server.c b/lib/sbi/server.c
+index 79789d0c9..3348a1798 100644
+--- a/lib/sbi/server.c
++++ b/lib/sbi/server.c
+@@ -30,9 +30,9 @@ static OGS_POOL(server_pool, ogs_sbi_server_t);
  void ogs_sbi_server_init(int num_of_session_pool, int num_of_stream_pool)
  {
      if (ogs_sbi_server_actions_initialized == false) {
@@ -32,8 +34,25 @@ if ! grep -q '/\* rt-5gms-applicatiopn-function patch applied \*/' "$open5gs_src
          ogs_sbi_server_actions = ogs_mhd_server_actions;
  #endif
      }
---- open5gs.orig/src/meson.build
-+++ open5gs/src/meson.build
+diff --git a/lib/sbi/support/20210629/openapi-generator/templates/model-header.mustache b/lib/sbi/support/20210629/openapi-generator/templates/model-header.mustache
+index 1f32ae27e..5e5af2155 100644
+--- a/lib/sbi/support/20210629/openapi-generator/templates/model-header.mustache
++++ b/lib/sbi/support/20210629/openapi-generator/templates/model-header.mustache
+@@ -16,6 +16,10 @@
+ #include "{{{.}}}.h"
+ {{/imports}}
+
++#define OpenAPI_{{classVarName}}_info_title "{{appName}}"
++#define OpenAPI_{{classVarName}}_info_version "{{appVersion}}"
++#define OpenAPI_{{classVarName}}_info_description "{{appDescription}}"
++
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+diff --git a/src/meson.build b/src/meson.build
+index d53fce06b..b512110eb 100644
+--- a/src/meson.build
++++ b/src/meson.build
 @@ -29,6 +29,8 @@ version_conf = configuration_data()
  version_conf.set_quoted('OPEN5GS_VERSION', package_version)
  configure_file(output : 'version.h', configuration : version_conf)
