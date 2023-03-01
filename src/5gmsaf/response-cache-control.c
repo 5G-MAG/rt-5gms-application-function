8125724937b42136ed67f5a18e189701b6bac729
deva <deva@rd.bbc.co.uk>
2023-03-01 14:45:45 +0000

    server response header modified

--- src/5gmsaf/response-cache-control.c
+++ src/5gmsaf/response-cache-control.c
@@ -10,11 +10,7 @@ https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
 
 #include "context.h"
 #include "response-cache-control.h"
-/*
-#define SERVER_RESPONSE_MAX_AGE 60
 
-#define M1_CONTENT_PROTOCOLS_RESPONSE_MAX_AGE 86400
-*/
 void msaf_server_response_cache_control_set(void)
 {
     msaf_server_response_cache_control_t *server_response_cache_control = NULL;
@@ -22,16 +18,17 @@ void msaf_server_response_cache_control_set(void)
     ogs_assert(server_response_cache_control);
     server_response_cache_control->m1_provisioning_session_response_max_age = SERVER_RESPONSE_MAX_AGE;
     server_response_cache_control->m1_content_hosting_configurations_response_max_age = SERVER_RESPONSE_MAX_AGE;
+    server_response_cache_control->m1_server_certificates_response_max_age = SERVER_RESPONSE_MAX_AGE;
     server_response_cache_control->m1_content_protocols_response_max_age = M1_CONTENT_PROTOCOLS_RESPONSE_MAX_AGE;
     server_response_cache_control->m5_service_access_information_response_max_age = SERVER_RESPONSE_MAX_AGE;
-
     msaf_self()->config.server_response_cache_control = server_response_cache_control;
 }
 
-void msaf_server_response_cache_control_set_from_config(int m1_provisioning_session_response_max_age, int m1_content_hosting_configurations_response_max_age, int m1_content_protocols_response_max_age, int m5_service_access_information_response_max_age)
+void msaf_server_response_cache_control_set_from_config(int m1_provisioning_session_response_max_age, int m1_content_hosting_configurations_response_max_age, int m1_server_certificates_response_max_age, int m1_content_protocols_response_max_age, int m5_service_access_information_response_max_age)
 {
     msaf_self()->config.server_response_cache_control->m1_provisioning_session_response_max_age = m1_provisioning_session_response_max_age;
     msaf_self()->config.server_response_cache_control->m1_content_hosting_configurations_response_max_age = m1_content_hosting_configurations_response_max_age;
+    msaf_self()->config.server_response_cache_control->m1_server_certificates_response_max_age = m1_server_certificates_response_max_age;
     msaf_self()->config.server_response_cache_control->m1_content_protocols_response_max_age = m1_content_protocols_response_max_age;
     msaf_self()->config.server_response_cache_control->m5_service_access_information_response_max_age = m5_service_access_information_response_max_age;
 }
