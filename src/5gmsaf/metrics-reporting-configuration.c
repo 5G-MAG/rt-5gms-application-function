6887789aca2602e4afea2575406d79f992dcaf24
stepski011 <vukbg.stojkovic@gmail.com>
2024-02-27 16:45:33 +0100

    POST updated, GET failing

--- src/5gmsaf/metrics-reporting-configuration.c
+++ src/5gmsaf/metrics-reporting-configuration.c
@@ -17,7 +17,6 @@ https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
 
 static char *calculate_metrics_reporting_configuration_hash(msaf_api_metrics_reporting_configuration_t *metricsReportingConfiguration);
 
-/* Auxiliary functions */
 ogs_hash_t * msaf_metrics_reporting_map(void)
 {
     ogs_hash_t *metrics_reporting_map = ogs_hash_make();
@@ -37,88 +36,80 @@ static char *calculate_metrics_reporting_configuration_hash(msaf_api_metrics_rep
     return metricsReportingConfiguration_hashed;
 }
 
-msaf_metrics_reporting_configuration_t *msaf_metrics_reporting_configuration_create(msaf_provisioning_session_t *provisioning_session,
-                                                                                    char *scheme,
-                                                                                    char *data_network_name,
-                                                                                    bool is_reporting_interval,
-                                                                                    int reporting_interval,
-                                                                                    bool is_sample_percentage,
-                                                                                    double sample_percentage,
-                                                                                    OpenAPI_list_t *url_filters,
-                                                                                    int sampling_period,
-                                                                                    OpenAPI_list_t *metrics) {
+ msaf_metrics_reporting_configuration_t* process_and_map_metrics_reporting_configuration(msaf_provisioning_session_t *provisioning_session, msaf_api_metrics_reporting_configuration_t *parsed_config) {
 
+     ogs_assert(provisioning_session);
+     ogs_assert(parsed_config);
 
-    ogs_uuid_t uuid;
-    char id[OGS_UUID_FORMATTED_LENGTH+1];
-    const char *parse_err = NULL;
-    ogs_assert(provisioning_session);
+     ogs_uuid_t uuid;
+     ogs_uuid_get(&uuid);
+     char new_id[OGS_UUID_FORMATTED_LENGTH + 1];
+     ogs_uuid_format(new_id, &uuid);
 
-    ogs_uuid_get(&uuid);
-    ogs_uuid_format(id, &uuid);
+     if (parsed_config->metrics_reporting_configuration_id != NULL) {
+         ogs_free(parsed_config->metrics_reporting_configuration_id);
+     }
+     parsed_config->metrics_reporting_configuration_id = msaf_strdup(new_id);
 
-    msaf_api_metrics_reporting_configuration_t *mrc = msaf_api_metrics_reporting_configuration_create(
-            msaf_strdup(id),
-            scheme,
-            data_network_name,
-            is_reporting_interval,
-            reporting_interval,
-            is_sample_percentage,
-            sample_percentage,
-            url_filters,
-            sampling_period,
-            metrics
-    );
+     msaf_metrics_reporting_configuration_t *msaf_metrics_config = ogs_calloc(1, sizeof(msaf_metrics_reporting_configuration_t));
 
-    if (mrc == NULL) {
-        ogs_error("Failed to create metrics reporting configuration: %s", parse_err);
-        return NULL;
-    }
-
-    /* Internal msaf_metrics_reporting_configuration object */
-    msaf_metrics_reporting_configuration_t *msaf_metrics_reporting_configuration = ogs_calloc(1, sizeof(msaf_metrics_reporting_configuration_t));
-    ogs_assert(msaf_metrics_reporting_configuration);
-
-    msaf_metrics_reporting_configuration->config = mrc;
-    msaf_metrics_reporting_configuration->config->metrics_reporting_configuration_id = msaf_strdup(id);
-    /* ETag & Received time */
-    msaf_metrics_reporting_configuration->etag = calculate_metrics_reporting_configuration_hash(msaf_metrics_reporting_configuration->config);
-    msaf_metrics_reporting_configuration->receivedTime = time(NULL);
-
-    /* Mapping */
-    if (provisioning_session->metrics_reporting_map == NULL) {
-        provisioning_session->metrics_reporting_map = msaf_metrics_reporting_map();
-    }
-    char *hashKey = msaf_strdup(msaf_metrics_reporting_configuration->config->metrics_reporting_configuration_id);
-    ogs_hash_set(provisioning_session->metrics_reporting_map,
-                 hashKey,
-                 OGS_HASH_KEY_STRING,
-                 msaf_metrics_reporting_configuration);
-
-    return msaf_metrics_reporting_configuration;
-}
-
-msaf_metrics_reporting_configuration_t* msaf_metrics_reporting_configuration_retrieve(const char *metrics_configuration_id) {
-
-     ogs_hash_index_t *provisioning_node;
-     ogs_hash_index_t *metrics_node;
-
-     if (!metrics_configuration_id) {
+     if (!msaf_metrics_config) {
+         ogs_error("Failed to allocate msaf_metrics_reporting_configuration");
          return NULL;
      }
 
-     for (provisioning_node = ogs_hash_first(msaf_self()->provisioningSessions_map); provisioning_node; provisioning_node = ogs_hash_next(provisioning_node)) {
-         msaf_provisioning_session_t *provisioning_session = ogs_hash_this_val(provisioning_node);
+     msaf_metrics_config->config = parsed_config;
+     msaf_metrics_config->etag = calculate_metrics_reporting_configuration_hash(msaf_metrics_config->config);
+     msaf_metrics_config->receivedTime = time(NULL);
 
-         for (metrics_node = ogs_hash_first(provisioning_session->metrics_reporting_map); metrics_node; metrics_node = ogs_hash_next(metrics_node)) {
-             char *currentMetricsId = (char *)ogs_hash_this_key(metrics_node);
-             if (strcmp(currentMetricsId, metrics_configuration_id) == 0) {
-                 return (msaf_metrics_reporting_configuration_t*)ogs_hash_this_val(metrics_node);
-             }
-         }
+     if (provisioning_session->metrics_reporting_map == NULL) {
+         provisioning_session->metrics_reporting_map = msaf_metrics_reporting_map();
      }
-     return NULL;
+
+     char *hashKey = msaf_strdup(msaf_metrics_config->config->metrics_reporting_configuration_id);
+     ogs_hash_set(provisioning_session->metrics_reporting_map, hashKey, OGS_HASH_KEY_STRING, msaf_metrics_config);
+
+     return msaf_metrics_config;
  }
 
+ msaf_metrics_reporting_configuration_t* msaf_metrics_reporting_configuration_retrieve(const msaf_provisioning_session_t *provisioning_session, const char *metrics_configuration_id) {
+     if (!provisioning_session || !metrics_configuration_id) {
+         return NULL;
+     }
+     return (msaf_metrics_reporting_configuration_t*)ogs_hash_get(provisioning_session->metrics_reporting_map, metrics_configuration_id, OGS_HASH_KEY_STRING);
+ }
 
+ cJSON *msaf_metrics_reporting_configuration_convertToJSON(const msaf_metrics_reporting_configuration_t *msaf_metrics_reporting_configuration) {
+
+     if (msaf_metrics_reporting_configuration == NULL || msaf_metrics_reporting_configuration->config == NULL) {
+         ogs_error("msaf_metrics_reporting_configuration_convertToJSON() failed [NULL pointer]");
+         return NULL;
+     }
+
+     cJSON *item = cJSON_CreateObject();
+
+     cJSON *configJSON = msaf_api_metrics_reporting_configuration_convertToJSON(msaf_metrics_reporting_configuration->config, false);
+     if (configJSON == NULL) {
+         ogs_error("msaf_metrics_reporting_configuration_convertToJSON() failed [config conversion]");
+         cJSON_Delete(item);
+         return NULL;
+     }
+     cJSON_AddItemToObject(item, "config", configJSON);
+
+     if (msaf_metrics_reporting_configuration->etag) {
+         if (cJSON_AddStringToObject(item, "etag", msaf_metrics_reporting_configuration->etag) == NULL) {
+             ogs_error("msaf_metrics_reporting_configuration_convertToJSON() failed [etag]");
+             cJSON_Delete(item);
+             return NULL;
+         }
+     }
+
+     if (cJSON_AddNumberToObject(item, "receivedTime", (double)msaf_metrics_reporting_configuration->receivedTime) == NULL) {
+         ogs_error("msaf_metrics_reporting_configuration_convertToJSON() failed [receivedTime]");
+         cJSON_Delete(item);
+         return NULL;
+     }
+
+     return item;
+ }
 
