1297be02a9ad737b39ae4d6ea74724d7dc5fc861
David Waring <david.waring2@bbc.co.uk>
2024-04-26 15:51:36 +0100

    Bug-fix: Incorrect return for consumption report configuration creation
    Fixes #156, #157.

--- src/5gmsaf/msaf-m1-sm.c
+++ src/5gmsaf/msaf-m1-sm.c
@@ -548,9 +548,9 @@ void msaf_m1_state_functional(ogs_fsm_t *s, msaf_event_t *e)
                                     } else {
                                         ogs_sbi_response_t *response;
 
-                                        response = nf_server_new_response(NULL, NULL,  0, NULL, 0, NULL, api, app_meta);
+                                        response = nf_server_new_response(message->h.uri, NULL,  0, NULL, 0, NULL, api, app_meta);
                                         ogs_assert(response);
-                                        nf_server_populate_response(response, 0, NULL, 204);
+                                        nf_server_populate_response(response, 0, NULL, 201);
                                         ogs_assert(true == ogs_sbi_server_send_response(stream, response));
                                     }
                                 }
